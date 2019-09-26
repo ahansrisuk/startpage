ef728fde2774afbe51a4787468434d28f5ea3cb3
Allen Hansrisuk <ahansrisuk@email.com>
2019-09-26 13:11:08 -0400

    update favicon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
 import '@/assets/icons/weather/flaticon.css';
 import '@/assets/css/tailwind.css';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
 
 const vm = new Vue({
   render: h => h(App),
