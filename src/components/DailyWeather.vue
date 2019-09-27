bb6efb51db3228fef726fa62fa907af0f71a56ef
Allen Hansrisuk <ahansrisuk@email.com>
2019-09-26 20:28:39 -0400

    finish weather components

---
+++ src/components/DailyWeather.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="ml-8 text-xs">
+    <span class="block" v-bind:class="getIcon(day.icon)"></span>
+    {{ day.high }}°/{{ day.low }}°
+</div>
+</template>
+
+<script>
+export default {
+    name: 'DailyWeather',
+    props:[
+        'day',
+    ],
+    methods: {
+        getIcon: function(icon) {
+            switch(icon){
+                case 'clear-day' || 'clear-night':
+                    return 'flaticon-016-sun';
+                    break;
+                case 'rain' || 'snow' || 'sleet':
+                    return 'flaticon-010-raining';
+                    break;
+                case 'wind' || 'fog':
+                    return 'flaticon-013-wind'
+                case 'partly-cloudy-day' || 'partly-cloudy-night':
+                    return 'flaticon-011-cloudy';
+                    break;
+            }
+        }
+    }
+}
+</script>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,24 +1,52 @@
 <template>
     <div class="flex">
-        <div class="ml-8 text-xs">
-            <span class="flaticon-016-sun block"></span>
-            74°/68°
-        </div>
-        <div class="ml-8 text-xs">
-            <span class="flaticon-010-raining"></span>
-            <p class="text-xs">74°/68°</p>
-        </div>
-        <div class="ml-8 text-xs">
-            <span class="flaticon-015-cloud"></span>
-            <p class="text-xs">74°/68°</p>
-        </div>
+            <DailyWeather v-for="day in forecast"
+                v-bind:day="day"
+                v-bind:key="day.id"
+            />
     </div>
 </template>
 
 <script>
 import auth from '../../auth.js';
+import DailyWeather from './DailyWeather.vue'
 
 export default {
     name: 'Weather',
+    components: {DailyWeather},
+    data: function() {
+        return {
+            forecast: []
+        }
+    },
+    methods: {
+        getWeather: function() {
+            const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+            const baseURI = 'https://api.darksky.net/forecast/';
+            const api_key = auth.darksky;
+            const request = corsProxy + baseURI + api_key + '/' + '39.32,-76.62';
+            this.$http.get(request)
+                .then((response) => {
+                    var weeklyForecast = response.data.daily.data;
+                    var i = 0;
+                    for (let i = 0; i < 3 ; i++) {
+                        var day = {};
+                        day.id = i;
+                        day.icon = weeklyForecast[i].icon;
+                        day.high = Math.round(weeklyForecast[i].apparentTemperatureHigh);
+                        day.low = Math.round(weeklyForecast[i].apparentTemperatureLow);
+                        this.forecast.push(day);
+
+                    }
+                    console.log(response);
+                }).catch((error) => {
+                    console.log(error);
+                })
+
+        }
+    },
+    mounted: function() {
+        this.getWeather();
+    }
 }
 </script>
